9381be9943aab8e8cff3100ff832372a3c3c2714
Ayokunle Paul <ayokunle@ulesson.com>
2020-07-05 18:26:22 +0100

    Simple project setup completed

--- app/src/main/java/com/ayokunlepaul/channel/MainActivity.kt
+++ app/src/main/java/com/ayokunlepaul/channel/MainActivity.kt
@@ -2,8 +2,12 @@ package com.ayokunlepaul.channel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+//    private val viewModel by lazy { ViewModelProviders }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
